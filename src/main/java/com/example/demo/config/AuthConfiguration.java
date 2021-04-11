21cea6a29ef539f876bd3c85eb47e6b406cf5a65
soanr <soanr@soanrdeMacBook-Pro.local>
2021-04-11 20:50:06 +0800

    添加拦截器

---
+++ src/main/java/com/example/demo/config/AuthConfiguration.java
@@ -0,0 +1,20 @@
+package com.example.demo.config;
+
+import com.example.demo.interceptor.LoginInterceptor;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
+
+@Configuration
+public class AuthConfiguration extends WebMvcConfigurationSupport {
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        InterceptorRegistration interceptor = registry.addInterceptor(new LoginInterceptor());
+        interceptor.addPathPatterns("/**");
+        interceptor.excludePathPatterns("/api/user/**");
+        super.addInterceptors(registry);
+    }
+
+}
