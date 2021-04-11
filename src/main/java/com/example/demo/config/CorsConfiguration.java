33705085054472bebb18dc45edd8cd79192c8812
soanr <soanr@soanrdeMacBook-Pro.local>
2021-04-11 22:09:13 +0800

    修复bug

---
+++ src/main/java/com/example/demo/config/CorsConfiguration.java
@@ -0,0 +1,19 @@
+package com.example.demo.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
+
+@Configuration
+public class CorsConfiguration extends WebMvcConfigurationSupport {
+
+    @Override
+    protected void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedHeaders("*")
+                .allowedMethods("*")
+                .allowedOrigins("*");
+        super.addCorsMappings(registry);
+    }
+
+}
